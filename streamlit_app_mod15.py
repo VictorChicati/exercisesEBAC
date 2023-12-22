d81990cb131171b96310024e5772769ffb4b196f
VictorChicati <145875780+VictorChicati@users.noreply.github.com>
2023-12-22 13:08:35 -0400

    Update streamlit_app_mod15.py
    Primeiro aplicativo - Documentação Streamlit, Uber app

--- streamlit_app_mod15.py
+++ streamlit_app_mod15.py
@@ -1,80 +1,36 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
+import numpy as np
 
-# st.write('Bem vinda')
+st.title('Uber pickups in NYC')
 
-# dataframe = np.random.randn(10, 20)
-# st.dataframe(dataframe)
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
 
-# dataframe = pd.DataFrame(
-#     np.random.randn(10, 20),
-#     columns=('col %d' % i for i in range(20)))
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache_data)")
 
-# st.dataframe(dataframe.style.highlight_max(axis=0))
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
 
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
 
-# dataframe = pd.DataFrame(
-#     np.random.randn(10, 20),
-#     columns=('col %d' % i for i in range(20)))
-# st.table(dataframe)
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-
-# x = st.slider('x')  # 👈 this is a widget
-# st.write(x, 'squared is', x * x)
-
-
-
-# st.title('My first app')
-
-# st.write("Here's our first attempt at using data to create a table:")
-# st.write(pd.DataFrame({
-#     'first column': [1, 2, 3, 4],
-#     'second column': [10, 20, 30, 40]
-# }))
-
-
-# """
-# # My first app
-# Here's our first attempt at using data to create a table:
-# """
-
-# df = pd.DataFrame({
-#   'first column': [1, 2, 3, 4],
-#   'second column': [10, 20, 30, 40]
-# })
-
-# df
-
-# chart_data = pd.DataFrame(
-#      np.random.randn(20, 3),
-#      columns=['a', 'b', 'c'])
-
-# st.line_chart(chart_data)
-
-
-# map_data = pd.DataFrame(
-#     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-#     columns=['lat', 'lon'])
-
-# st.map(map_data)
-
-
-
-
-import time
-
-'Starting a long computation...'
-
-# Add a placeholder
-latest_iteration = st.empty()
-bar = st.progress(0)
-
-for i in range(100):
-  # Update the progress bar with each iteration.
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
-
-'...and now we\'re done!'
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
